d466f48bd603df59f71460d1baad28728bb204f6
peeCaffeine <axljohnmendoza@gmail.com>
2024-04-19 19:45:03 +0800

    added Types

---
+++ Rawri/Types.hh
@@ -0,0 +1,22 @@
+#ifndef __RAWRI__TYPES__HH__
+#define __RAWRI__TYPES__HH__
+
+#include <cstdint>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+using f32 = float;
+using f64 = double;
+
+using usize = size_t;
+using umax = uintmax_t;
+
+#endif // __RAWRI__TYPES__HH__
